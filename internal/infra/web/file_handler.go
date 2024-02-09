a2596e0af4aaf2fd4c97f3d06b7deba75386e252
Brendo Vale <brendo.vale02@gmail.com>
2024-02-09 10:45:59 -0400

    Implement file handler

---
+++ internal/infra/web/file_handler.go
@@ -0,0 +1,46 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/bvaledev/go-filemanager/internal/domain"
+)
+
+type FileHandler struct {
+	FileService domain.FileService
+}
+
+func NewFileHandler(FileService domain.FileService) *FileHandler {
+	return &FileHandler{FileService}
+}
+
+type output struct {
+	Failed []string `json:"failed_uploads"`
+}
+
+func (h *FileHandler) Upload(w http.ResponseWriter, r *http.Request) {
+	// maximum upload of 40 MB.
+	if err := r.ParseMultipartForm(40 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filesToUpload := make([]domain.FileInfo, 0)
+	for _, fileHeader := range r.MultipartForm.File["files"] {
+		file, _ := fileHeader.Open()
+		defer file.Close()
+		fileInfo := domain.FileInfo{
+			Name: fileHeader.Filename,
+			File: file,
+		}
+		filesToUpload = append(filesToUpload, fileInfo)
+	}
+
+	failed := h.FileService.UploadFiles(filesToUpload)
+
+	if err := json.NewEncoder(w).Encode(output{Failed: failed}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
